3a278da679b0fb21600cb6da23fa64517e13b648
Caleb Moses <calebjdmoses@gmail.com>
2022-07-14 14:26:12 +1200

    Add start_cluster.sh

---
+++ start_cluster.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -ex
+
+echo Setting environment variables using .env..
+source .env
+
+echo Prepare infrastructure configuration..
+bash pt_serve_util.sh
+
+echo Create the cluster using eksctl..
+eksctl create cluster -f ${K8S_MANIFESTS_DIR}/cluster.yaml
+
+echo Deploy Pods to EKS cluster..
+kubectl create namespace ${NAMESPACE}
+kubectl -n ${NAMESPACE} apply -f ${K8S_MANIFESTS_DIR}/pt_inference.yaml
+
+echo Showing pods..
+kubectl get pods -n ${NAMESPACE}
