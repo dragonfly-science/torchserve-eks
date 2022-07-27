d4965d87b0356fca2f242cc8b9d8a82d385e736c
Caleb Moses <calebjdmoses@gmail.com>
2022-07-27 14:36:14 +1200

    Update eks deployment

--- delete_cluster.sh
+++ delete_cluster.sh
@@ -5,4 +5,4 @@ source .env
 
 NODE_INSTANCE_ROLE_NAME=$(eksctl get iamidentitymapping --cluster $AWS_CLUSTER_NAME | tail -1 | cut -f1  | cut -f2 -d/)
 aws iam delete-role-policy --role-name ${NODE_INSTANCE_ROLE_NAME} --policy-name cw-log-policy
-eksctl delete cluster --name ${AWS_CLUSTER_NAME}
+eksctl delete cluster --name ${AWS_CLUSTER_NAME} --wait
--- register_models.sh
+++ register_models.sh
@@ -4,7 +4,6 @@ set -ex
 
 echo Register Models with TorchServe
 EXTERNAL_IP=`kubectl get svc -n ${NAMESPACE} -o jsonpath='{.items[0].status.loadBalancer.ingress[0].hostname}'`
-response=$(curl --write-out %{http_code} --silent --output /dev/null --retry 5 -X POST "http://${EXTERNAL_IP}:8081/models?url=https://torchserve.s3.amazonaws.com/mar_files/resnet-18.mar&initial_workers=1&synchronous=true")
 
 if [ ! "$response" == 200 ]
 then
@@ -12,3 +11,5 @@ then
 else
     echo "successfully registered model with torchserve"
 fi
+
+eksctl scale nodegroup --cluster iwk-t2-medium --nodes=5 ng-1 --nodes-min 1 --nodes-max 10
